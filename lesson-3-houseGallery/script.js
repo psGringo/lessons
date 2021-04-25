c9309218d03fd0fdcd7338753951280fc341fbf1
panteleevstas <panteleevstas@gmail.com>
2021-04-25 23:44:34 +0500

    Домашнее задание "Сверстать галерею домов"

--- lesson-3-houseGallery/script.js
+++ lesson-3-houseGallery/script.js
@@ -1,18 +1,19 @@
-// console.log(navigator.userAgent, navigator.platform)
-console.dir('fontSize', document.body.style.fontSize.toString());
-
-function getDefaultFontSize(pa){
-    pa= pa || document.body;
-    const who= document.createElement('div');
-
-    who.style.cssText='display:inline-block; padding:0; line-height:1; position:absolute; visibility:hidden; font-size:1em';
-
-    who.appendChild(document.createTextNode('M'));
-    pa.appendChild(who);
-    var fs= [who.offsetWidth, who.offsetHeight];
-    pa.removeChild(who);
-    return fs;
+function changeHousesLeftMargin(marginOffset) {
+    let marginLeft = parseInt(document.getElementById('house1').style.marginLeft, 10) || 0;
+    marginLeft += marginOffset;
+    document.getElementById('house1').style.marginLeft = marginLeft.toString() + 'px';
+    const progressValue = Math.random() * 100;
+    document.getElementById('progress').setAttribute('value', progressValue.toString());
 }
 
-console.log(getDefaultFontSize());
+const houseWidth = document.getElementById('house1').clientWidth;
 
+const buttonRight = document.getElementsByClassName('sliderNavigation__buttons_right')[0];
+buttonRight.addEventListener('click', () => {
+    changeHousesLeftMargin(houseWidth);
+});
+
+const buttonLeft = document.getElementsByClassName('sliderNavigation__buttons_left')[0];
+buttonLeft.addEventListener('click', () => {
+    changeHousesLeftMargin(-houseWidth);
+});
