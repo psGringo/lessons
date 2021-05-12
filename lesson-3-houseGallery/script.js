f1e08c4a0441e4aa00743f89b962d2d9ee061efa
Panteleev Stas <SOPanteleev@sberbank.ru>
2021-05-13 03:46:48 +0500

    refactoring

--- lesson-3-houseGallery/script.js
+++ lesson-3-houseGallery/script.js
@@ -1,10 +1,41 @@
-const firstHouse = document.getElementById('house1');
-const lastHouse = document.getElementById('house4');
-const progressBar = document.getElementById('progressBar');
-const progressLeftBorder = progressBar.getBoundingClientRect().left;
-const progressRightBorder = progressBar.getBoundingClientRect().right;
-const buttonLeft = document.getElementsByClassName('navigationButtons-left')[0];
-const buttonRight = document.getElementsByClassName('navigationButtons-right')[0];
+const houses = document.querySelectorAll('.house');
+const firstHouse = houses[0];
+const lastHouse = houses[houses.length - 1];
+
+const progressControl = document.querySelector('progress');
+let progressValue = 0;
+const buttonLeft = document.querySelector('.buttons__left');
+const buttonRight = document.querySelector('.buttons__right');
+
+let leftBorder;
+let rightBorder;
+
+function getBorders() {
+    leftBorder = progressControl.getBoundingClientRect().left;
+    rightBorder = document.querySelector('.buttons__right').getBoundingClientRect().right;
+}
+
+function getProgress() {
+    return lastHouse.getBoundingClientRect().right - buttonRight.getBoundingClientRect().right;
+}
+
+let maxProgressValue = getProgress();
+const totalHousesWidth = getHousesRightBorder() - getHousesLeftBorder();
+updateControlsState();
+
+function shiftLeftMargin(offset) {
+    let marginLeft = parseInt(firstHouse.style.marginLeft, 10) || 0;
+    marginLeft += offset;
+
+    if (isNoNeedUpdateLeftMargin(offset)) return
+    else
+        doShiftLeftMargin(marginLeft);
+}
+
+function doShiftLeftMargin(value) {
+    firstHouse.style.marginLeft = value.toString() + 'px';
+    updateControlsState();
+}
 
 function getHousesRightBorder() {
     return lastHouse.getBoundingClientRect().right;
@@ -14,42 +45,68 @@ function getHousesLeftBorder() {
     return firstHouse.getBoundingClientRect().left
 }
 
-let totalHousesWidth = getHousesRightBorder() - progressRightBorder;
-disableNavButtonsIfNeeded(0);
-
-function changeHousesLeftMargin(marginOffset) {
-    let marginLeft = parseInt(document.getElementById('house1').style.marginLeft, 10) || 0;
-    marginLeft += marginOffset;
-
-    if ((marginOffset > 0) && (getHousesLeftBorder() >= progressLeftBorder) || (marginOffset < 0) && (getHousesRightBorder() <= progressRightBorder))
-        return;
-
-    // -- shift
-    firstHouse.style.marginLeft = marginLeft.toString() + 'px';
-    const currentProgress = (1 - ((getHousesRightBorder() - progressRightBorder) / totalHousesWidth)) * 100;
-    disableNavButtonsIfNeeded(currentProgress);
-    document.getElementById('progressBar').setAttribute('value', currentProgress.toString());
+function isNoNeedUpdateLeftMargin(marginOffset) {
+    return ((marginOffset > 0) && (getHousesLeftBorder() >= leftBorder) || ((marginOffset < 0) && (getHousesRightBorder() <= rightBorder)))
 }
 
-function disableNavButtonsIfNeeded(currentProgress) {
-    if (currentProgress == 0) {
-        buttonLeft.setAttribute("style", "background-color: antiquewhite;");
-    } else
-        buttonLeft.setAttribute("style", "background-color: #1C4088;");
-
-    if (currentProgress >= 100) {
-        buttonRight.setAttribute("style", "background-color: antiquewhite;");
-    } else
-        buttonRight.setAttribute("style", "background-color: #1C4088;");
+function disableButton(button) {
+    button.setAttribute("style", "background-color: burlywood;");
 }
 
-const shiftWidth = document.getElementById('house1').clientWidth / 2;
+function enableButton(button) {
+    button.setAttribute("style", "background-color: #1C4088;");
+}
+
+function updateProgressControl() {
+    progressValue = (1 - (getProgress() / maxProgressValue)) * 100;
+    document.querySelector('progress').setAttribute('value', progressValue.toString());
+}
+
+function getWindowWidth() {
+    return window.innerWidth
+        || document.documentElement.clientWidth
+        || document.body.clientWidth;
+}
+
+function updateButtonsState() {
+
+    if (progressValue === 0) disableButton(buttonLeft)
+    else
+        enableButton(buttonLeft);
+
+    if ((progressValue >= 100) || (lastHouse.getBoundingClientRect().right <= getWindowWidth()))
+        disableButton(buttonRight)
+    else
+        enableButton(buttonRight);
+}
+
+function updateControlsState() {
+    updateProgressControl();
+    updateButtonsState();
+}
+
+const shiftWidth = firstHouse.clientWidth / 2;
 
 buttonRight.addEventListener('click', () => {
-    changeHousesLeftMargin(-shiftWidth);
+    shiftLeftMargin(-shiftWidth);
 });
 
 buttonLeft.addEventListener('click', () => {
-    changeHousesLeftMargin(shiftWidth);
+    shiftLeftMargin(shiftWidth);
 });
+
+function initialize() {
+    maxProgressValue = getProgress();
+    getBorders();
+    doShiftLeftMargin(0);
+    updateButtonsState(0);
+}
+
+window.addEventListener('resize', (event) => {
+    initialize();
+})
+
+window.addEventListener('load', (event) => {
+    initialize();
+})
 
